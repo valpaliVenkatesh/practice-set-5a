1504ab192a2863a1512847dbabd3acc37339bdda
valpaliVenkatesh <valpalivenkatesh1@gmail.com>
2022-05-04 14:06:19 +0000

    api3

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const path = require("path");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const app = express();
+app.use(express.json());
 
 const dbPath = path.join(__dirname, "moviesData.db");
 
 let db = null;
+
+function sendDbToJsonResponse(movie) {
+  return {
+    movieId: movie.movie_id,
+    directorId: movie.director_id,
+    movieName: movie.movie_name,
+    leadActor: movie.lead_actor,
+  };
+}
 
 const initializeDBAndServer = async () => {
   try {
@@ -42,13 +52,37 @@ app.get("/movies/", async (request, response) => {
 });
 //API2
 app.post("/movies/", async (request, response) => {
-  const { directorId, movieName, leadActor } = request.body;
-  const postMovieQuery = `
+  try {
+    const { directorId, movieName, leadActor } = request.body;
+    const postMovieQuery = `
   INSERT INTO
     movie ( director_id, movie_name, lead_actor)
   VALUES
     (${directorId}, '${movieName}', '${leadActor}');`;
-  await database.run(postMovieQuery);
-  response.send("Movie Successfully Added");
+    await db.run(postMovieQuery);
+    response.send("Movie Successfully Added");
+  } catch (e) {
+    console.log(`${e.message}`);
+  }
+});
+
+//API3
+app.get("/movies/:movieId/", async (request, response) => {
+  try {
+    const { movieId } = request.params;
+    const getMovieQuery = `
+    SELECT
+      *
+    FROM
+      movie
+    WHERE
+      movie_id = ${movieId};`;
+    const movie = await db.get(getMovieQuery);
+    response.send(sendDbToJsonResponse(movie));
+  } catch (e) {
+    console.log(`${e.message}`);
+  }
 });
 module.exports = app;
+
+//API4
